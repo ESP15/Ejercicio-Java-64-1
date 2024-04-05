a6a8da655aa481763bc999f57d9052700897211e
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-05 09:53:36 +0200

    Add files via upload

---
+++ javaaprende1/main.js
@@ -0,0 +1,22 @@
+/*PARTE 1 DEL EJERCICIO */
+
+const nombre = ( nombre, apellido ) => {
+    return nombre +' '+ apellido ;
+};
+
+console.log (nombre ("Eric","Sanz Peiri"))
+
+/* PARTE 2 DEL EJERCICIO */
+
+let boolean = false;
+function nombre (Eric){
+    Eric = true
+}
+
+
+/* PARTE 3 DEL EJERCICIO */
+
+const funcioninfinita = [ "1","2","3","4","5"];
+    funcioninfinita.forEach(element => {
+        console.log(element)
+    });
